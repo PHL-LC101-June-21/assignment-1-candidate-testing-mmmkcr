70554acc7c09cc0c2f5dd8fe0dc240597236430f
Ladan Zaim (she/her) <52222385+mmmkcr@users.noreply.github.com>
2021-06-13 22:02:48 +0000

    Added code to Pt 1 sections. Having issues returning correct grade initially, moved candidateAnswer declaration lower down to the 1.2c section. Had a hard time finding info on functions / user input for JS

--- candidate-testing.js
+++ candidate-testing.js
@@ -3,11 +3,12 @@ const input = require('readline-sync');
 // TODO 2: modify your quiz app to ask 5 questions //
 
 // TODO 1.1a: Define candidateName // 
-let candidateName;
+let candidateName = "";
+
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question;
-let correctAnswer;
-let candidateAnswer;
+let question = "Who was the first American woman in space? ";
+let correctAnswer = "Sally Ride";
+let candidateAnswer = "";
 let questions;
 let correctAnswers;
 let candidateAnswers;
@@ -15,23 +16,27 @@ let candidateAnswers;
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
-
+let candidateName = input.question("Enter your name: ");
+console.log("Hello " + candidateName + "!" );
 }
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-
-
+//let candidateAnswer = input.question(question); 
+//^^^ need to figure out the role of this piece... could not get code to run with it in. Tested with different names returning false as expected
 }
 
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
 
-
-  let grade;
-  
-
+let candidateAnswer = input.question(question);
+let grade = true;
+if (candidateAnswer === correctAnswer) {
+  console.log("Correct");
+} else {
+  console.log("Incorrect");
+}
   return grade;
 }
 
